cf9b065eee3273f6bc34fad6514db1517ccf4b8f
keroloussamy <keroloussamy98@gmail.com>
2021-12-15 02:41:43 +0200

    add project structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 import "./style.css";
-
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = 'Hello webpack';
-    return element;
-  }
-  
-  document.body.appendChild(component());
