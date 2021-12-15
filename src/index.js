79bfe5978e8412351d1752716359c9e386ad6dc7
keroloussamy <keroloussamy98@gmail.com>
2021-12-16 01:24:41 +0200

    Add API Calls

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import CRUD from './CRUD.js';
+
+document.querySelector('.refreshBtn').addEventListener('click', CRUD.displayScores);
+document.querySelector('.postScoreForm').addEventListener('submit', (event) => {
+  CRUD.GetScoreFormValues();
+  event.preventDefault();
+});
